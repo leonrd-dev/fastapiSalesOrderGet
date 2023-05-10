aa7f957547fc9d30d23e85a60f249d543e55924f
leonrd-dev <132048668+leonrd-dev@users.noreply.github.com>
2023-05-10 16:53:02 +0700

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 #server = 'nomad2805'
 #database = 'DMSTEST'
 
-database_url = f"mssql+pymssql://leonardi:Internship2023@10.1.32.64/DMSLIVE" #MSSQL SERVER
+database_url = f"mssql+pymssql:// "#MSSQL SERVER
 #database_url = 'mssql+pyodbc://' + server + '/' + database + '?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server'
 #database_url = f"postgresql://{check.db_user}:{check.db_password}@{check.db_server}:{check.db_port}/{check.db_name}" #POSTGRESQL
 
