bc89b06db3d246cc26b5d47c13d6bdb85c4721f6
leonrd-dev <132048668+leonrd-dev@users.noreply.github.com>
2023-05-26 10:05:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ app = FastAPI()
 
 router = APIRouter(tags=["Get Sales Order"],prefix="/SalesOrder")
 
-@router.get("/get-sales_order", status_code=200)
+@router.get("/get-sales_order", status_code=status.HTTP_200_OK)
 def get_sales_order(db:Session=Depends(get_db)):
     sales_order = salesordercrud.get_all_sales_order_crud(db,0,100)
     if not sales_order:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     return sales_order
 
-@router.get("/get-sales-order/{SO_SYS_NO}", status_code=200)
+@router.get("/get-sales-order/{SO_SYS_NO}", status_code=status.HTTP_200_OK)
 def get_sales_order(SO_SYS_NO, db:Session=Depends(get_db)):
     sales_order = salesordercrud.get_one_sales_order_crud(db, SO_SYS_NO)
     if not sales_order:
