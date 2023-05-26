bbd5c3a330bae320e0b1552ff17616d2670b8ba9
leonrd-dev <132048668+leonrd-dev@users.noreply.github.com>
2023-05-26 09:54:14 +0700

    Update salesordercrud.py

--- salesordercrud.py
+++ salesordercrud.py
@@ -9,17 +9,3 @@ def get_all_sales_order_crud(db:Session, offset:int=0, limit:int=100):
 def get_one_sales_order_crud(db:Session,get_id:int):
     return  db.query(modelorm.SO_Header).filter(modelorm.SO_Header.so_sys_no==get_id).first()
     
-def post_sales_order(db:Session, payload:modelorm.SO_Header):
-    return modelorm.SO_Header(**payload.dict())
-
-def delete_sales_order(db:Session,get_id:int):
-    return db.query(modelorm.SO_Header).filter(modelorm.SO_Header.so_sys_no==get_id).delete(synchronize_session=False)
-    
-def put_sales_order(db:Session,payload:modelorm.SO_Header, get_id:int):
-    edit_sales_order= db.query(modelorm.SO_Header).filter(modelorm.SO_Header.so_sys_no==get_id)
-    edit_sales_order.update(payload.dict())
-    messages_sales_order = edit_sales_order.first()
-    return edit_sales_order, messages_sales_order
-
-def patch_sales_order(db:Session, get_id:int):
-    return db.query(modelorm.SO_Header).filter(modelorm.SO_Header.so_sys_no==get_id).first()
