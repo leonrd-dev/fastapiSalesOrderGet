b1114b216b6d36f9dbe27efa3c5aced7de03a6f5
leonrd-dev <132048668+leonrd-dev@users.noreply.github.com>
2023-05-31 17:12:18 +0700

    Update salesordercrud.py

--- salesordercrud.py
+++ salesordercrud.py
@@ -1,11 +1,43 @@
-import modelorm
+import model
 from sqlalchemy.orm import Session
+import schema
 
 
 
-def get_all_sales_order_crud(db:Session, offset:int=0, limit:int=100):
-    return db.query(modelorm.SO_Header).order_by(modelorm.SO_Header.so_sys_no).offset(offset).limit(limit).all()
+def get_detail_sales_order_crud(db:Session, offset:int=0, limit:int=100):
 
-def get_one_sales_order_crud(db:Session,get_id:int):
-    return  db.query(modelorm.SO_Header).filter(modelorm.SO_Header.so_sys_no==get_id).first()
+    detail = db.query(model.SO_Detail).order_by(model.SO_Detail.SO_SYS_NO).offset(offset).limit(limit).all()
+
+    return detail
+   
+
+
+
+
+def get_header_sales_order_crud(db:Session,get_id:int):
+    header = db.query(model.SO_Header).filter(model.SO_Header.SO_SYS_NO==get_id).order_by(model.SO_Header.SO_SYS_NO).first()
+    return header
+def get_all_sales_order(db:Session, get_id:int,offset:int=0, limit:int=100):
+    header = db.query(model.SO_Header).filter(model.SO_Header.SO_SYS_NO==get_id).order_by(model.SO_Header.SO_SYS_NO).first()
+    # detail = db.query(model.SO_Header).filter(model.SO_Detail.so_sys_no==get_id).order_by(model.SO_Header.so_sys_no).all()
+    details = db.query(model.SO_Detail).filter(model.SO_Detail.SO_SYS_NO == get_id).order_by(model.SO_Detail.SO_SYS_NO).all()
+
+    # header_schema = SO_Header.from_orm(header)
+    # detail_schema = SO_Detail.from_orm(detail)
+
+    # header_schema.Item.append(detail_schema)
+    header_schema = schema.SO_Header.from_orm(header)
+    detail_schemas = [schema.SO_Detail.from_orm(row) for row in details]
+
+   
+    for detail_schema in detail_schemas:
+        header_schema.Item.append(detail_schema)
+
+   
+
+   
+
+    return header_schema
+
+
     
