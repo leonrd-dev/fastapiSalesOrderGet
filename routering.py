fdc02a52c4df813539c363b3ea8558fdda7ebec0
leonrd-dev <132048668+leonrd-dev@users.noreply.github.com>
2023-05-31 17:10:20 +0700

    Create routering.py

---
+++ routering.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter,Depends,HTTPException,status
+import schema
+import salesordercrud
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+from database import get_db
+
+
+router = APIRouter(tags=["Get Sales Order"],prefix="/SalesOrder")
+
+@router.get("/get-header-sales-orders/{SO_SYS_NO}", status_code=status.HTTP_200_OK)
+def get_sales_orders(SO_SYS_NO, db:Session=Depends(get_db)):
+    sales_order = salesordercrud.get_header_sales_order_crud(db,SO_SYS_NO)
+    if not sales_order:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return sales_order
+
+@router.get("/get-detail-sales-order/{SO_SYS_NO}", status_code=status.HTTP_200_OK)
+def get_sales_orders(SO_SYS_NO, db:Session=Depends(get_db)):
+    sales_order = salesordercrud.get_detail_sales_order_crud(db,SO_SYS_NO)
+    if not sales_order:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return sales_order
+
+@router.get("/get-sales-order/{SO_SYS_NO}", status_code=status.HTTP_200_OK)
+def get_sales_order(SO_SYS_NO, db:Session=Depends(get_db)):
+    sales_order = salesordercrud.get_all_sales_order(db, SO_SYS_NO)
+    if not sales_order:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return sales_order
+
+
+RouterSO = router
+
+
