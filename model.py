8ad75a7bb59ec35ebda79701e23c3ee47e5d4235
leonrd-dev <132048668+leonrd-dev@users.noreply.github.com>
2023-05-31 17:11:32 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,31 +1,35 @@
-from typing import List, Optional
-from pydantic import BaseModel
-from decimal import Decimal
-import datetime
+from sqlalchemy import Column, Integer, String, Date, Numeric, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+from typing import List
 
-class SO_Detail(BaseModel):
-    ITEM_CODE: str
-    QTY_DEMAND: Decimal
-    DISC_PERCENT: Decimal
-    DISC_AMOUNT: Decimal
-    QTY_SUPPLY: Decimal
-    SO_SYS_NO: int
 
-    class Config :
-        orm_mode = True
 
-class SO_Header(BaseModel):
-    SO_SYS_NO: int
-    SO_DOC_NO: str
-    SO_DATE: datetime.date
-    SO_STATUS: str
-    CUST_CODE: str
-    TOP_CODE: str
-    TOTAL: Decimal
-    SALES_EMP_NO: Optional[str] = None
-    # Item:Optional[List[SO_Detail]] = []
-    Item: List[SO_Detail] = []
 
-    class Config :
-        orm_mode = True
+
+Base = declarative_base()
+
+class SO_Detail(Base):
+    __tablename__ = 'atSalesOrder1'
+    SO_LINE_NO = Column(Integer, primary_key=True)
+    ITEM_CODE = Column(String)
+    QTY_DEMAND  = Column(Numeric)
+    DISC_PERCENT = Column(Numeric)
+    DISC_AMOUNT = Column(Numeric)
+    QTY_SUPPLY = Column(Numeric)
+    SO_SYS_NO = Column(Integer, ForeignKey('atSalesOrder0.SO_SYS_NO'))
+    header = relationship("SO_Header", back_populates="items")
+
+
+class SO_Header(Base):
+    __tablename__ = 'atSalesOrder0'
+    SO_SYS_NO = Column(Integer, primary_key=True)
+    SO_DOC_NO = Column(String)
+    SO_DATE = Column(Date)
+    SO_STATUS = Column(String)
+    CUST_CODE = Column(String)
+    TOP_CODE = Column(String)
+    TOTAL = Column(Numeric)
+    SALES_EMP_NO = Column(String, nullable=True)
+    items = relationship("SO_Detail", back_populates="header")
+
 
